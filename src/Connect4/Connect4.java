48ae4884c1135e2381700b89b91768a6b40b8324
Peter Alexander <peter.alexander@securitease.com>
2017-12-04 11:14:42 +1300

    added comments for the interface and vertical validation works for the game

--- src/Connect4/Connect4.java
+++ src/Connect4/Connect4.java
@@ -3,23 +3,23 @@ package Connect4;
 public class Connect4 {
 	int[][] grid = new int[7][6];
 	int currentPlayer = 1;
-	
+
 	public Connect4() {
 		grid[0][0] = 1;
-//		grid[0][1] = 1;
-//		grid[0][2] = 1;
-//		grid[0][4] = 1;
-		
+		// grid[0][1] = 1;
+		// grid[0][2] = 1;
+		// grid[0][4] = 1;
+
 		System.out.println(isGameOver());
 	}
-	
+
 	private boolean makeMove(int col, int player) {
-		for(int i = 0; i < 6; i++) {
-			if(i == 5 && grid[i][col] != 0) {
+		for (int i = 0; i < 6; i++) {
+			if (i == 5 && grid[i][col] != 0) {
 				System.out.println("Invalid move, there is no space in that column");
 				return false;
 			}
-			if(grid[i][col] == 0) {
+			if (grid[i][col] == 0) {
 				grid[i][col] = player;
 				isGameOver();
 				return true;
@@ -28,45 +28,52 @@ public class Connect4 {
 		System.out.println("Something bust, flame the developer.");
 		return false;
 	}
-	
+
 	private boolean isGameOver() {
-		//check verticals
-		for(int x = 0; x < 7; x++) {
+		// check verticals
+		for (int x = 0; x < 7; x++) {
 			int chainLength = 0;
 			int player = grid[x][0];
-			
-			for(int y = 0; y < 6; y++) {
+
+			for (int y = 0; y < 6; y++) {
 				System.out.println(x + ", " + y + ": " + grid[x][y]);
-				if(grid[x][y] == player) {
+				if (grid[x][y] == player) {
 					chainLength++;
-				}
+				} 
 				else {
 					System.out.println("hi");
 					chainLength = 1;
 					player = grid[x][y];
 				}
-				if(chainLength == 4 && player != 0)return true;
+				
+				if (chainLength == 4 && player != 0)
+					return true;
 			}
 		}
-		
-		//check horizontals
-//		for(int y = 0; y < 6; y++) {
-//			int chainLength = 0;
-//			int player = grid[0][y];
-//			
-//			for(int x = 0; x < 7; x++) {
-//				if(grid[x][y] == player)chainLength++;
-//				else {
-//					chainLength = 1;
-//					player = grid[x][y];
-//				}
-//				if(chainLength == 4)return true;
-//			}
-//		}
-		
+
+		// check horizontals
+		for (int y = 0; y < 6; y++) {
+			int chainLength = 0;
+			int player = grid[0][y];
+
+			for (int x = 0; x < 7; x++) {
+				if (grid[x][y] == player) {
+					chainLength++;
+				} 
+				else {
+					System.out.println("hi");
+					chainLength = 1;
+					player = grid[x][y];
+				}
+				
+				if (chainLength == 4 && player != 0)
+					return true;
+			}
+		}
+
 		return false;
 	}
-	
+
 	public static void main(String[] args) {
 		new Connect4();
 	}
--- src/Connect4/Connect4AI.java
+++ src/Connect4/Connect4AI.java
@@ -2,6 +2,12 @@ package Connect4;
 
 public interface Connect4AI {
 	
+	/**
+	 * chooses a move to make, returns the column to place a token in
+	 * (ie it works like people playing connect 4 where you drop a token into a column)
+	 * @param board
+	 * @return
+	 */
 	public int chooseMove(int[][] board);
 	
 }
